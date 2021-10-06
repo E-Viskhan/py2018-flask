87df0205d6f814090e0483054cf5fbb1595f59c2
MSI\zelim <zelimdamian@gmail.com>
2021-10-06 13:59:44 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, jsonify
+import sqlite3
+from flask import g
+
+DATABASE = 'database.sqlite'
+
+app = Flask(__name__)
+
+
+def make_dicts(cursor, row):
+    return dict((cursor.description[idx][0], value)
+                for idx, value in enumerate(row))
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = make_dicts
+    return db
+
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+@app.route('/contacts')
+def main():
+    results = query_db("select * from contacts")
+    return jsonify({"data": results})
+
+
+if __name__ == '__main__':
+    init_db()
+    app.run(port=3000)
---
+++ schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS contacts (
+	id INTEGER PRIMARY KEY,
+	first_name TEXT NOT NULL,
+	last_name TEXT NOT NULL,
+	email TEXT NOT NULL UNIQUE,
+	phone TEXT NOT NULL UNIQUE
+);
